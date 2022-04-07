01db7dc86f80d72f5d391864b076f454cd1095cb
Jeff <powersosig@riseup.net>
2022-04-07 21:51:38 +0200

    Add action button component

---
+++ view/components/ButtonAction.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <a v-if="isExternal" target="_blank" :href="link" class="action-button">
+      {{ text }}
+      <slot />
+    </a>
+    <nuxt-link v-else :to="link" class="action-button">
+      {{ text }}
+      <slot />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonAction',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: false,
+      default: '#'
+    },
+    isExternal: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .action-button {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 8px 8.5px;
+    gap: 6px;
+    background: var(--primary-green);
+    border-radius: 8px;
+  }
+</style>
