d295231c455e5138767112b58f924bf1c6510eab
Jeff <powersosig@riseup.net>
2022-04-07 03:56:46 +0200

    Add application page (entry point for app)

---
+++ view/pages/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    a
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPage',
+  layout (context) {
+    return context.$device.isMobile ? 'mobile' : 'desktop'
+  },
+  data () {
+    return {
+      test: false
+    }
+  }
+}
+</script>
