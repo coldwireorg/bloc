c6ab474ad6d0a94232fd9f8b3db8fb1e024e3362
Jeff <powersosig@riseup.net>
2022-04-07 03:56:05 +0200

    Add desktop layout

---
+++ view/layouts/desktop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <AppSideBar />
+    <div class="appcontent-rsp-container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DesktopLayout'
+}
+</script>
+
+<style scoped>
+  .appcontent-rsp-container {
+    position: absolute;
+    top: 0;
+    left: 254px;
+    right: 0;
+    bottom: 0;
+    background-color: #161B22;
+  }
+</style>
