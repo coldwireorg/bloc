2bf88125126925224a95f9d9bd470ac73e86e701
Jeff <powersosig@riseup.net>
2022-04-08 00:19:52 +0200

    Add green and blue color support and external link condition for action button comp

--- view/components/ButtonAction.vue
+++ view/components/ButtonAction.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <a v-if="isExternal" target="_blank" :href="link" class="action-button">
+    <a v-if="isExternal" target="_blank" :href="link" class="action-button" :class="getClassColor()">
       {{ text }}
       <slot />
     </a>
-    <nuxt-link v-else :to="link" class="action-button">
+    <nuxt-link v-else :to="link" class="action-button" :class="getClassColor()">
       {{ text }}
       <slot />
     </nuxt-link>
@@ -28,6 +28,21 @@ export default {
       type: Boolean,
       required: false,
       default: false
+    },
+    color: {
+      type: String,
+      required: false,
+      default: 'green'
+    }
+  },
+
+  methods: {
+    getClassColor () {
+      if (this.color === 'blue' || this.color === 'BLUE') {
+        return 'prm-blue'
+      } else {
+        return 'prm-green'
+      }
     }
   }
 }
@@ -40,9 +55,16 @@ export default {
     justify-content: flex-start;
     align-items: center;
     box-sizing: border-box;
-    padding: 8px 8.5px;
+    padding: 8px 9px;
     gap: 6px;
-    background: var(--primary-green);
     border-radius: 8px;
+  }
+
+  .prm-green {
+    background: var(--primary-green);
+  }
+
+  .prm-blue {
+    background: var(--primary-blue);
   }
 </style>
