f99ed3862214374805f51f7185fdfb5e6842d9aa
Jeff <powersosig@riseup.net>
2022-04-18 02:51:24 +0200

    Add mime type to app file type converter (#10)

--- view/js/converters.js
+++ view/js/converters.js
@@ -1,5 +1,5 @@
-export const dateToLapsedTime = (date) => {
-  const seconds = Math.floor((new Date() - new Date(date)) / 1000)
+export const dateToLapsedTime = (_date) => {
+  const seconds = Math.floor((new Date() - new Date(_date)) / 1000)
   let interval = seconds / 31536000
   if (interval > 1) {
     return 'Edited ' + Math.floor(interval) + ' years ago'
@@ -25,4 +25,22 @@ export const dateToLapsedTime = (date) => {
     return 'Edited ' + Math.floor(interval) + ' minutes ago'
   }
   return 'Edited recently'
+}
+
+export const mimeTypeToAppType = (_mimetype) => {
+  if (/image\/*/.test(_mimetype)) {
+    return 'image'
+  } else if (/video\/*/.test(_mimetype)) {
+    return 'video'
+  } else if (/audio\/*/.test(_mimetype)) {
+    return 'audio'
+  } else if (/application\/vnd.ms-excel*/.test(_mimetype) || _mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || _mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.template') {
+    return 'classor'
+  } else if (/application\/vnd\.openxmlformats-officedocument.presentationml\.*/.test(_mimetype) || _mimetype === 'application/vnd.ms-powerpoint') {
+    return 'presentation'
+  } else if (/application\/vnd\.ms-word\.*/.test(_mimetype) || /application\/vnd\.openxmlformats-officedocument\.wordprocessingml\.*/.test(_mimetype || /application\/msword*/.test(_mimetype))) {
+    return 'document'
+  } else {
+    return 'unknow'
+  }
 }
